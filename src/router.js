a5edfcc83985efd5b8710a6f3d5c0912dac30fd6
AnanthaKannan <sreeananthakannan@gmail.com>
2021-03-20 12:37:32 +0530

    css changed

--- src/router.js
+++ src/router.js
@@ -1,18 +1,11 @@
 import React, { createContext, useReducer } from 'react'
 import Dummy from './pages/Dummy'
-// import Login from './pages/Login'
-// import Dashboard from './pages/Dashboard'
-// import TiltStation from './pages/TiltStation';
 
 export const routes = [
     {
         COMPONENT: <Dummy />,
         PATH: '/dummy'
     },
-    // {
-    //     COMPONENT: <Login />,
-    //     PATH: '/login'
-    // }, 
     {
         COMPONENT: <Dummy />,
         PATH: '/'

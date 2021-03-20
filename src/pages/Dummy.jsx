5306237d73ddc7a7952f0a52d483a8466dbf2502
AnanthaKannan <sreeananthakannan@gmail.com>
2021-03-21 02:41:03 +0530

    inital page created

--- src/pages/Dummy.jsx
+++ src/pages/Dummy.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import TestComp from '../components/TestComp'
+import AgGirdReact from '../reusable/AgGirdReact'
 import H1 from '../reusable/H1'
 import H2 from '../reusable/H2'
+import MiniDrawer from '../reusable/MiniDrawer';
+import Typography from '@material-ui/core/Typography';
 
 export default function Dummy() {
     return (
         <div>
-            <TestComp />
-            <H1 text='some data here' />
-            <H2 text='h2 data is here' />
+           <MiniDrawer>
+
+           <Typography variant="h6" className='mb-2'> Product List</Typography>
+            <AgGirdReact />
+            </MiniDrawer>
         </div>
     )
 }

c112acea31bc6eb0a9d099c9f2959a7ef1a3d049
AnanthaKannan <sreeananthakannan@gmail.com>
2021-03-20 12:32:55 +0530

    basic structure created

---
+++ src/pages/Dummy.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import TestComp from '../components/TestComp'
+
+export default function Dummy() {
+    return (
+        <div>
+            <TestComp />
+        </div>
+    )
+}
--- src/router.js
+++ src/router.js
@@ -1,23 +1,21 @@
 import React, { createContext, useReducer } from 'react'
-import Login from './pages/Login'
-import Dashboard from './pages/Dashboard'
-import TiltStation from './pages/TiltStation';
+import Dummy from './pages/Dummy'
+// import Login from './pages/Login'
+// import Dashboard from './pages/Dashboard'
+// import TiltStation from './pages/TiltStation';
 
 export const routes = [
     {
-        COMPONENT: <TiltStation />,
-        PATH: '/tilt-station'
+        COMPONENT: <Dummy />,
+        PATH: '/dummy'
     },
+    // {
+    //     COMPONENT: <Login />,
+    //     PATH: '/login'
+    // }, 
     {
-        COMPONENT: <Dashboard />,
-        PATH: '/dashboard'
-    },
-    {
-        COMPONENT: <Login />,
-        PATH: '/login'
-    }, {
-        COMPONENT: <Login />,
+        COMPONENT: <Dummy />,
         PATH: '/'
-    },
+    }
 
 ]
